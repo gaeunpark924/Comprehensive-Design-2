1952a1695fcf442e259b92f0e51831e53fab664a
gaeunpark924 <51811995+gaeunpark924@users.noreply.github.com>
2021-11-08 02:17:56 +0900

    Update config.js

--- nodejs/config.js
+++ nodejs/config.js
@@ -1,11 +1,12 @@
 var config = {};
 
 //Get this value from https://grabz.it/api
-config.applicationKey = "MjI1ZThjMDg1MjM2NDA4ZTlkODNmMzQ3ZDQ2ZWE1YWY=";
-
+config.applicationKey = "";
 //Get this value from https://grabz.it/api
-config.applicationSecret = "Pz84VCI/PyJ1PwsIP2cGPz8/Dw8/PzVcPz9mQT8/PwM=";
+config.applicationSecret = "";
 //The absolute URL that of your handler on your website
-config.callbackHandlerUrl = 'http://11.222.33.44:8080/handler';
+config.callbackHandlerUrl = "";
+//Your ip
+config.ip = "";
 
 module.exports = config;
